2edc22c0835dd3fbf30f6a0bd068b52126c9c5ac
LeoDiD <148693769+LeoDiD@users.noreply.github.com>
2025-01-28 16:17:47 +0800

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Auth, Orders } from './pages';
+import { Header } from './components/shared';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-        <h1 className='text-red-500 underline'>Hello</h1>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/orders' element={<Orders />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
