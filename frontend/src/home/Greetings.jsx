575777771a96953cbcb8ea068ccc5dea9e60779e
LeoDiD <148693769+LeoDiD@users.noreply.github.com>
2025-01-30 21:53:54 +0800

    home ui

---
+++ frontend/src/home/Greetings.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const Greetings = () => {
+    const [dateTime, setDateTime] = useState(new Date());
+
+    const formatDate = (date) => {
+        const months = [
+            'January', 'February', 'March', 'April', 'May', 'June', 
+            'July', 'August', 'September', 'October', 'November', 'December'
+        ];
+        return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}, ${date.toLocaleTimeString()}`;
+    };
+
+    const formatTime = (date) => 
+        `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()
+        ).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+
+    return (
+        <div className='flex justify-between items-center px-8'>
+            <div>
+                <h1 className='text-[#f5f5f5] text-2xl font-semibold tracking-wide'>Good Morning, Leovi</h1>
+                <p className='text-[#ababab] text-sm'>Give your best services for customers</p>
+            </div>
+            <div>
+                <h1>{dateTime.toLocaleTimeString()}</h1>
+                <p>{formatDate(dateTime)}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Greetings;
