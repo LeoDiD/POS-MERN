c9714ca684aa307c55a1d0674def464a04ecd591
LeoDiD <148693769+LeoDiD@users.noreply.github.com>
2025-03-13 21:57:46 +0800

    add another component in home ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React from 'react';
 import BottomNav from '../components/BottomNav';
 import Greetings from '../home/Greetings';
 import MiniCard from '../home/MiniCard';
 import { BsCashCoin } from 'react-icons/bs';
 import { GrinProgress } from 'react-icons/gr';
+import RecentOrder from '../home/RecentOrder';
 
 const Home = () => {
   return (
     <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
-      <div className='flex[3]'></div>
+      <div className='flex-1'></div> {/* Adjusted flex class */}
       <Greetings />
       <div className='flex items-center w-full gap-3 px-8 mt-8'>
-        <Minicard title="Total Earnings" icon={<BsCashCoin />} number={512}
-        footerNum={1.6} />
-        <Minicard title="In Progress" icon={<GrinProgress />} number={16}
-        footerNum={3.6} />
+        <MiniCard 
+          title="Total Earnings" 
+          icon={<BsCashCoin />} 
+          number={512} 
+          footerNum={1.6} 
+        />
+        <MiniCard 
+          title="In Progress" 
+          icon={<GrinProgress />} 
+          number={16} 
+          footerNum={3.6} 
+        />
       </div>
-      <div className='flex[2] bg-blue-600'></div>
+
+    <RecentOrder />
+
+      <div className='flex-[2] bg-blue-600'></div> {/* Adjusted flex class */}
       <BottomNav />
     </section>
   );
 };
 
-export default Home
+export default Home;
